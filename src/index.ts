6b4768a7e2e38492d28fe1abe6997eb93fb1e54b
Eugene <FitChiON@gmail.com>
2024-09-08 02:19:21 +0300

    fix: exports in index.js

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-import { SteamTotp } from './lib/steamtotp';
-
-const getCode = async () => {
-  const code = await new SteamTotp().generateSteamTotp(
-    'uB08PvpZey9gnlgjywrQbIr3dcc='
-  );
-
-  console.log(code);
-};
-
-getCode();
+export * from './lib/steamtotp';
